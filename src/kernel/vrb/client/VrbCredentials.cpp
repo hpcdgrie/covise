e123c44a9dbda64a3df12f01bb3459d9e8c2edf0
hpcdgrie <dennis.grieger@hlrs.de>
2021-01-07 14:08:48 +0100

    Implement operator== + fixes

--- src/kernel/vrb/client/VrbCredentials.cpp
+++ src/kernel/vrb/client/VrbCredentials.cpp
@@ -23,3 +23,7 @@ covise::TokenBuffer &vrb::operator<<(covise::TokenBuffer &tb, const VrbCredentia
     tb << cred.ipAddress << static_cast<int>(cred.tcpPort) << static_cast<int>(cred.udpPort);
     return tb;
 }
+
+bool vrb::operator==(const VrbCredentials& cred1, const VrbCredentials& cred2){
+    return cred1.ipAddress == cred2.ipAddress && cred1.tcpPort == cred2.tcpPort && cred1.udpPort == cred2.udpPort;
+}
--- src/kernel/vrb/client/VrbCredentials.h
+++ src/kernel/vrb/client/VrbCredentials.h
@@ -9,15 +9,16 @@ namespace covise
 }
 namespace vrb
 {
-    struct VRBCLIENTEXPORT VrbCredentials
-    {
-        VrbCredentials(const std::string &ipAddress, unsigned int tcpPort, unsigned int udpPort);
-        VrbCredentials();
-        const std::string ipAddress;
-        const unsigned int tcpPort;
-        const unsigned int udpPort;
-    };
-    VRBCLIENTEXPORT covise::TokenBuffer &operator<<(covise::TokenBuffer &, const VrbCredentials &cred);
+struct VRBCLIENTEXPORT VrbCredentials
+{
+    VrbCredentials(const std::string &ipAddress, unsigned int tcpPort, unsigned int udpPort);
+    VrbCredentials();
+    const std::string ipAddress;
+    const unsigned int tcpPort;
+    const unsigned int udpPort;
+};
+VRBCLIENTEXPORT covise::TokenBuffer &operator<<(covise::TokenBuffer &, const VrbCredentials &cred);
+VRBCLIENTEXPORT bool operator==(const VrbCredentials &cred1, const VrbCredentials &cred2);
 
 } // namespace vrb
 
@@ -27,8 +28,8 @@ namespace detail{
 template<>
 inline vrb::VrbCredentials get(covise::TokenBuffer &tb){
     std::string ip;
-    int tcp, udp;
-    tb >> ip, tcp, udp;
+    int tcp = 0, udp = 0;
+    tb >> ip >> tcp >> udp;
     return vrb::VrbCredentials{ip, static_cast<unsigned int>(tcp), static_cast<unsigned int>(udp)};
 }
 }
