b47e32426b3a9262fe87d8f8931db2bade42fde6
hpcdgrie <dennis.grieger@hlrs.de>
2021-01-29 10:06:43 +0100

    Added COVISE_MESSAGE_NEW_UI

---
+++ src/kernel/comsg/NEW_UI.cpp
@@ -0,0 +1,21 @@
+#include "NEW_UI.h"
+#include <net/message.h>
+#include <net/message_sender_interface.h>
+#include <net/message_types.h>
+#include <net/tokenbuffer.h>
+#include <net/tokenbuffer_serializer.h>
+#include <net/tokenbuffer_util.h>
+namespace covise{
+
+IMPL_MESSAGE_WITH_SUB_CLASSES(NEW_UI, NEW_UI_TYPE)
+
+IMPL_SUB_MESSAGE_CLASS(NEW_UI, NEW_UI_TYPE, HandlePartners, 
+    LaunchStyle, launchStyle,
+    int, timeout,
+    std::vector<int>, clients)
+
+IMPL_SUB_MESSAGE_CLASS(NEW_UI, NEW_UI_TYPE, RequestAvailablePartners, int, dummy)
+IMPL_SUB_MESSAGE_CLASS(NEW_UI, NEW_UI_TYPE, AvailablePartners, ClientList, clients)
+
+
+} //covise
---
+++ src/kernel/comsg/NEW_UI.h
@@ -0,0 +1,31 @@
+
+
+#include "coviseLaunchOptions.h"
+
+#include <net/message_macros.h>
+#include <util/coExport.h>
+
+#include <vector>
+#include <utility>
+
+#define COMMA ,
+namespace covise{
+enum class NEW_UI_TYPE
+{
+    HandlePartners,
+    RequestAvailablePartners,
+    AvailablePartners
+};
+DECL_MESSAGE_WITH_SUB_CLASSES(NEW_UI, NEW_UI_TYPE, COMSGEXPORT)
+
+DECL_SUB_MESSAGE_CLASS(NEW_UI, NEW_UI_TYPE, HandlePartners, COMSGEXPORT,
+    LaunchStyle, launchStyle,
+    int, timeout,
+    std::vector<int>, clients)
+
+
+DECL_SUB_MESSAGE_CLASS(NEW_UI, NEW_UI_TYPE, RequestAvailablePartners, COMSGEXPORT, int, dummy)
+typedef std::vector<std::pair<int, std::string>> ClientList;
+DECL_SUB_MESSAGE_CLASS(NEW_UI, NEW_UI_TYPE, AvailablePartners, COMSGEXPORT, ClientList, clients)
+
+}//covise
