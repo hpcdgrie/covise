fabe0d060f5635ba2292ea1dad7f1cb20aab1bfd
hpcdgrie <dennis.grieger@hlrs.de>
2021-03-01 09:04:33 +0100

    Updated tests

--- tests/CRB_EXEC.cpp
+++ tests/CRB_EXEC.cpp
@@ -10,18 +10,17 @@ void test_crbExec() {
 	params.push_back("test8");
 	params.push_back("test9");
 
-	covise::CRB_EXEC crbExec1{ covise::ExecFlag::Memcheck, "test1", 31000, "test2", 5, "test3", "test4", "test5", nullptr, nullptr, 13, vrb::VrbCredentials{"TEST", 83211, 934782}, params };
+	covise::CRB_EXEC crbExec1{ covise::ExecFlag::Memcheck, "test1", 31000, "test2", 5, "test3", "test4", "test5", nullptr, 13, vrb::VrbCredentials{"TEST", 83211, 934782}, params };
 	//std::cerr << crbExec1 << std::endl << std::endl;
 	auto a = covise::getCmdArgs(crbExec1);
 	auto args = covise::cmdArgsToCharVec(a);
 	covise::CRB_EXEC crbExec2 = covise::getExecFromCmdArgs(a.size(), const_cast<char**>(args.data()));
 	//std::cerr << crbExec2 << std::endl << std::endl;
 	assert(!strcmp(crbExec1.name, crbExec2.name));
-	assert(!strcmp(crbExec1.localIp, crbExec2.localIp));
+	assert(!strcmp(crbExec1.controllerIp, crbExec2.controllerIp));
 	assert(!strcmp(crbExec1.moduleId, crbExec2.moduleId));
 	assert(!strcmp(crbExec1.moduleIp, crbExec2.moduleIp));
 	assert(!strcmp(crbExec1.moduleHostName, crbExec2.moduleHostName));
-	assert(!crbExec2.displayIp);
 	assert(!crbExec2.category);
 	assert(crbExec1.params[0] == crbExec2.params[0]);
 	assert(crbExec1.params[1] == crbExec2.params[1]);
