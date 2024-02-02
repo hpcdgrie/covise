839b3e47abac84aef849144db1ee8ecbb3c289dd
hpcdgrie <grieger@hlrs.de>
2024-02-02 10:04:39 +0100

    avatar tests

--- src/OpenCOVER/plugins/general/Avatar/Bone.cpp
+++ src/OpenCOVER/plugins/general/Avatar/Bone.cpp
@@ -1,99 +1,99 @@
 #include "Bone.h"
-#include <cover/ui/Menu.h>
-#include <cover/ui/Button.h>
-#include <cover/ui/Menu.h>
-
 #include <osgAnimation/UpdateBone>
+#include <osgAnimation/Bone>
+#include <osgAnimation/StackedRotateAxisElement>
 #include <array>
 
-using namespace opencover;
-
-BoneFinder::BoneFinder(const std::string &name) 
+BoneParser::BoneParser() 
 : osg::NodeVisitor(osg::NodeVisitor::TRAVERSE_ALL_CHILDREN)
-, m_nodeName(name) {}
-
-
-void BoneFinder::apply(osg::Node& node) {
-
-    // std::cerr << "nodename " << node.getName();
-    node.getUpdateCallback();
-    // if(dynamic_cast<osgAnimation::Skeleton*>(&node))
-    //     std::cerr << " is a skeleton";
-    // if(dynamic_cast<osgAnimation::Bone*>(&node))
-    //     std::cerr << " is a Bone";
-    // if(dynamic_cast<osgAnimation::RigGeometry*>(&node))
-    //     std::cerr << " is a RigGeometry";
-    // std::cerr << std::endl;
-    if(node.getName() == m_nodeName)
-    {
-        bone = dynamic_cast<osgAnimation::Bone*>(&node);
-        return;
-    }
-    traverse(node);
+, ikSolver(ik.solver.create(IK_FABRIK)) {
+    /* We want to calculate rotations as well as positions */
+    ikSolver->flags = 0; //only calc positions or we get a weird mix of positions and rotations
+    ikSolver->flags |= IK_ENABLE_TARGET_ROTATIONS; 
+    ikSolver->flags |= IK_ENABLE_CONSTRAINTS; 
 }
 
-AnimationManagerFinder::AnimationManagerFinder() 
-: osg::NodeVisitor(osg::NodeVisitor::TRAVERSE_ALL_CHILDREN) {}
+void BoneParser::apply(osg::Node& node) {
 
-void AnimationManagerFinder::apply(osg::Node& node) {
-
-    if (m_am.valid())
-        return;
-
-    if (node.getUpdateCallback()) {       
-        m_am = dynamic_cast<osgAnimation::BasicAnimationManager*>(node.getUpdateCallback());
-        return;
-    }
-    
-    traverse(node);
-}
-
-float getBoneLenght(osgAnimation::UpdateBone *bone)
-{
-    if(bone)
+    if(auto bone = dynamic_cast<osgAnimation::Bone*>(&node))
     {
-        for (auto i = bone->getStackedTransforms().begin(); i < bone->getStackedTransforms().end(); i++)
+        std::cerr << "bone: " << bone->getName() << std::endl;
+        Bone *parent = nullptr;
+        ik_node_t *ikNode;
+        if(!bone->getBoneParent())
         {
-            if((*i)->getName() == "translate")
+            root = ikSolver->node->create(ikId++);
+            ikNode = root;
+        }
+        else
+        {
+            parent = &nodeToIk[bone->getBoneParent()];
+            ikNode = ikSolver->node->create_child(parent->ikNode, ikId++);
+        }
+        // auto constraint = ikSolver->constraint->create(IK_CUSTOM);
+        // ikSolver->constraint->set_custom(constraint, [](ik_node_t* node){
+        //     auto b = (Bone*) node->user_data;
+        //     std::cerr << "constraint: " << b->osgNode->getName() << std::endl;
+        //     return 1;
+        // });
+        // auto attachResult = ikSolver->constraint->attach(constraint, ikNode);
+
+        auto &stacked = dynamic_cast<osgAnimation::UpdateBone*>(node.getUpdateCallback())->getStackedTransforms();
+        osgAnimation::StackedTranslateElement *ste = nullptr;
+        for (const auto &i : stacked)
+        {
+            if(auto translate = dynamic_cast<osgAnimation::StackedTranslateElement*>(i.get()))
             {
-                auto translate = dynamic_cast<osgAnimation::StackedTranslateElement*>(i->get());
                 auto t = translate->getTranslate();
-                return t.length();
+                ikNode->position = ik.vec3.vec3(t.x(), t.y(), t.z());
+                ste = translate;
+            } 
+        }
+        
+        auto sqe = new osgAnimation::StackedQuaternionElement;
+        Bone &ikBone = nodeToIk.emplace(std::make_pair(&node, Bone{sqe, ste, parent, ikNode, &node})).first->second;
+        stacked.push_back(sqe);
+        ikNode->user_data = &ikBone;
+        for (size_t i = 0; i < effectorName.size(); i++)
+        {
+            if(node.getName() == effectorName[i])
+            {
+                size_t chainLength = effectorChainLenghts[i];
+                auto origin = bone;
+                for (size_t i = 0; i < chainLength + 1; i++)
+                {
+                    auto &stacked =  dynamic_cast<osgAnimation::UpdateBone*>(origin->getUpdateCallback())->getStackedTransforms();
+                    stacked.erase(std::remove_if(stacked.begin(), stacked.end(), [](const osgAnimation::StackedTransform::value_type& t){
+                        return dynamic_cast<osgAnimation::StackedRotateAxisElement*>(t.get());
+                    }), stacked.end());
+                    origin = origin->getBoneParent();
+                }
+                effectors[i] = std::make_unique<Effector>(effectorName[i], ikNode, chainLength, origin, ikSolver);
             }
         }
     }
-    return 0;
+    traverse(node);
 }
 
-void removeOrientationAndScale(osgAnimation::UpdateBone *bone)
+BoneParser::NodeMap::iterator BoneParser::findNode(const std::string &name)
 {
-    for (auto i = bone->getStackedTransforms().begin(); i < bone->getStackedTransforms().end(); i++)
-    {
-        if((*i)->getName() != "translate")
-        {
-            i = bone->getStackedTransforms().erase(i);
-        }
-    }
+    return std::find_if(nodeToIk.begin(), nodeToIk.end(), [&name](const NodeMap::value_type &p){
+        return p.first->getName() == name;
+    });
 }
 
-void replaceAnimationRotation(osgAnimation::Bone *bone, osgAnimation::StackedQuaternionElement* rotationToUse)
+osg::Vec3 BoneParser::claculateBoneDistance(const std::string &boneName1, const std::string &boneName2)
 {
-    auto cb = dynamic_cast<osgAnimation::UpdateBone*>(bone->getUpdateCallback());
-    if(cb)
-    {
-        removeOrientationAndScale(cb);
-        cb->getStackedTransforms().push_back(rotationToUse);
-    }
-}
-
-Bone::Bone(Bone *parent, const std::string &name, osg::Node* model, ui::Menu *menu)
-:rotation(new osgAnimation::StackedQuaternionElement("quaternion", osg::Quat(0, osg::Y_AXIS)))
-, parent(parent)
-, name(name)
-{
-    BoneFinder bf(name);
-    model->accept(bf);
-    bone = bf.bone;
-    replaceAnimationRotation(bone, rotation);
-    length = getBoneLenght(dynamic_cast<osgAnimation::UpdateBone*>(bone->getChild(0)->getUpdateCallback()));
+    auto bone1it = findNode(boneName1);
+    auto bone2it = findNode(boneName2);
+    auto rootIt = findNode("mixamorig:Hips");
+    if(bone1it == nodeToIk.end() || bone2it == nodeToIk.end() || rootIt == nodeToIk.end())
+        return osg::Vec3(0,0,0);
+    
+    const osg::Node *bone1 = bone1it->first;
+    const osg::Node *bone2 = bone2it->first;
+    const osg::Node *root = rootIt->first;
+    auto bone1Pos = bone1->getWorldMatrices(root)[0].getTrans();
+    auto bone2Pos = bone2->getWorldMatrices(root)[0].getTrans();
+    return bone2Pos - bone1Pos;
 }
