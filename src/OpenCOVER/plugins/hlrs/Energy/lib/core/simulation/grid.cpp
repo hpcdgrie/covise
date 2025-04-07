c6e23f873cbc5739ab0dd821dfb47ba688c705e0
MDjur <marko.djuric@gmx.de>
2025-04-07 09:41:46 +0200

    EnergyCampus: add Lines to grid

--- src/OpenCOVER/plugins/hlrs/Energy/lib/core/simulation/grid.cpp
+++ src/OpenCOVER/plugins/hlrs/Energy/lib/core/simulation/grid.cpp
@@ -2,13 +2,13 @@
 
 #include <utils/osgUtils.h>
 
+#include <algorithm>
 #include <osg/MatrixTransform>
 #include <osg/Shape>
 
 namespace core::simulation::grid {
 Point::Point(const std::string &name, const float &x, const float &y, const float &z,
              const float &radius, const Data &additionalData)
-    // : osg::Group(),
     : osg::MatrixTransform(),
       m_point(new osg::Sphere(osg::Vec3(x, y, z), radius)),
       m_additionalData(additionalData) {
@@ -36,15 +36,54 @@ DirectedConnection::DirectedConnection(const std::string &name,
                                        const osg::Vec3 &start, const osg::Vec3 &end,
                                        const float &radius,
                                        osg::ref_ptr<osg::TessellationHints> hints,
-                                       const Data &additionalData)
-    // : osg::Group(),
+                                       const Data &additionalData,
+                                       ConnectionType type)
     : osg::MatrixTransform(),
       m_start(new osg::Vec3(start)),
       m_end(new osg::Vec3(end)),
       m_additionalData(additionalData) {
+  switch (type) {
+    case ConnectionType::Line:
   m_geode = utils::osgUtils::createCylinderBetweenPoints(
       start, end, radius, osg::Vec4(1.0f, 1.0f, 0.0f, 1.0f), hints);
+      break;
+    case ConnectionType::Arc:
+      m_geode = utils::osgUtils::createBezierTube(
+          start, end, radius * 2.0f, radius, 50, osg::Vec4(1.0f, 1.0f, 0.0f, 1.0f));
+      break;
+    case ConnectionType::Arrow:
+      assert(false && "Arrow type not implemented");
+  }
   addChild(m_geode);
   setName(name);
 }
+
+Line::Line(std::string name, const Connections &connections) : m_name(name) {
+  init(connections);
+}
+
+void Line::init(const std::vector<osg::ref_ptr<DirectedConnection>> &connections) {
+  if (connections.empty()) {
+    std::cerr << "Line: No connections provided\n";
+    return;
+  }
+  setName(m_name);
+  // TODO: for now only the first connection is used to get the additional data
+  m_additionalData = connections[0]->getAdditionalData();
+  for (const auto &connection : connections) {
+    const auto &name = connection->getName();
+    m_connections[name] = connection;
+    addChild(connection);
+  }
+}
+
+bool Line::operator==(const Line &other) const {
+  return m_name == other.m_name ||
+         std::any_of(m_connections.begin(), m_connections.end(),
+                     [&other](const auto &pair) {
+                       return other.m_connections.find(pair.first) !=
+                              other.m_connections.end();
+                     });
+}
+
 }  // namespace core::simulation::grid
--- src/OpenCOVER/plugins/hlrs/Energy/lib/core/simulation/grid.h
+++ src/OpenCOVER/plugins/hlrs/Energy/lib/core/simulation/grid.h
@@ -4,12 +4,14 @@
 #include <osg/Group>
 #include <osg/MatrixTransform>
 #include <osg/ShapeDrawable>
+#include <osg/Vec3>
 #include <variant>
 
 #include "../utils/color.h"
 
 namespace core::simulation::grid {
-// grid::Data is a map of std::string and variants that can hold int, float, or string
+// grid::Data is a map of std::string and variants that can hold int, float, or
+// string
 typedef std::map<std::string, std::variant<float, int, std::string>> Data;
 class Point : public osg::MatrixTransform {
  public:
@@ -18,7 +20,7 @@ class Point : public osg::MatrixTransform {
   Point(const Point &p);
 
   const auto &getPosition() const { return m_point->getCenter(); }
-  const Data &getAdditionalData() const { return m_additionalData; }
+  const auto &getAdditionalData() const { return m_additionalData; }
   osg::ref_ptr<osg::Geode> getGeode() {
     return dynamic_cast<osg::Geode *>(osg::Group::getChild(0));
   }
@@ -54,27 +56,37 @@ struct ConnectionData {
   Data additionalData;
 };
 
-// class DirectedConnection : public osg::Group {
+enum class ConnectionType { Line, Arc, Arrow };
+
 class DirectedConnection : public osg::MatrixTransform {
   DirectedConnection(const std::string &name, const osg::Vec3 &start,
                      const osg::Vec3 &end, const float &radius,
                      osg::ref_ptr<osg::TessellationHints> hints,
-                     const Data &additionalData = Data());
+                     const Data &additionalData = Data(),
+                     ConnectionType type = ConnectionType::Line);
 
  public:
-  DirectedConnection(const ConnectionData<osg::Vec3> &data)
+  DirectedConnection(const ConnectionData<osg::Vec3> &data,
+                     ConnectionType type = ConnectionType::Line)
       : DirectedConnection(data.name, data.start, data.end, data.radius, data.hints,
                            data.additionalData) {};
 
-  DirectedConnection(const ConnectionData<Point> &data)
+  DirectedConnection(const ConnectionData<Point> &data,
+                     ConnectionType type = ConnectionType::Line)
       : DirectedConnection(data.name, data.start.getPosition(),
                            data.end.getPosition(), data.radius, data.hints,
                            data.additionalData) {};
+  ~DirectedConnection() {
+    delete m_start;
+    delete m_end;
+  }
 
   osg::Vec3 getDirection() const { return *m_end - *m_start; }
   osg::Vec3 getCenter() const { return (*m_start + *m_end) / 2; }
+  osg::Vec3 getStart() const { return *m_start; }
+  osg::Vec3 getEnd() const { return *m_end; }
   osg::ref_ptr<osg::Geode> getGeode() const { return m_geode; }
-  const Data &getAdditionalData() const { return m_additionalData; }
+  const auto &getAdditionalData() const { return m_additionalData; }
   void updateColor(const osg::Vec4 &color) {
     core::utils::color::overrideGeodeColor(m_geode, color);
   }
@@ -84,20 +96,39 @@ class DirectedConnection : public osg::MatrixTransform {
   osg::Vec3 *m_start;
   osg::Vec3 *m_end;
   Data m_additionalData;
+  ConnectionType m_type;
 };
 
-// class Line {
-//  private:
-//   std::map<std::string, osg::ref_ptr<DirectedConnection>> m_connections;
-// };
-
-typedef std::vector<osg::ref_ptr<Point>> Points;
-typedef std::vector<osg::ref_ptr<DirectedConnection>> Connections;
 // list of directed connections between points
+typedef std::vector<osg::ref_ptr<DirectedConnection>> Connections;
+
+class Line : public osg::MatrixTransform {
+ public:
+  Line(std::string name, const Connections &connections);
+
+  const auto &getConnections() const { return m_connections; }
+  const auto &getAdditionalData() const { return m_additionalData; }
+  osg::Vec3 getCenter() const {
+    osg::Vec3 center(0.0f, 0.0f, 0.0f);
+    for (const auto &[_, connection] : m_connections)
+      center += connection->getCenter();
+    return center / m_connections.size();
+  }
+  bool operator==(const Line &other) const;
+
+ private:
+  void init(const Connections &connections);
+
+  std::string m_name;
+  std::map<std::string, osg::ref_ptr<DirectedConnection>> m_connections;
+  Data m_additionalData;
+};
+
 // TODO: write a concept for PointType
 // template <typename PointType>
 // using ConnectivityList = std::vector<ConnectionData<PointType>>;
 typedef std::vector<osg::ref_ptr<Point>> Points;
+typedef std::vector<osg::ref_ptr<Line>> Lines;
 typedef std::vector<std::vector<int>> Indices;
 typedef std::vector<Data> PointDataList;
 typedef std::vector<std::vector<Data>> ConnectionDataList;
