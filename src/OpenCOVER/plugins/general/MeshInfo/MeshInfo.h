98c9dad8cf50f662a30932d4917771d76a03eaa8
hpcdgrie <grieger@hlrs.de>
2025-06-30 16:08:47 +0200

    MeshInfo test

---
+++ src/OpenCOVER/plugins/general/MeshInfo/MeshInfo.h
@@ -0,0 +1,47 @@
+/* This file is part of COVISE.
+
+   You can use it under the terms of the GNU Lesser General Public License
+   version 2.1 or later, see lgpl-2.1.txt.
+
+ * License: LGPL 2+ */
+#ifndef _MESHINFO_H
+#define _MESHINFO_H
+
+ #include <osg/Node>
+ #include <osg/MatrixTransform>
+ #include <osg/Material>
+ #include <osg/Geode>
+ #include <osg/ShapeDrawable>
+ #include <osgText/Text>
+ #include <cover/coVRPluginSupport.h>
+ 
+ 
+ 
+ #include <array>
+ #include <vector>
+ 
+ 
+ using namespace vrui;
+ using namespace opencover;
+ 
+ 
+ /** Line between markers */
+ 
+ 
+ /** Main plugin class */
+ class MeshInfo : public coVRPlugin
+ {
+ public:
+ 
+    MeshInfo();
+ 
+ 
+ private:
+ 
+
+ 
+ 
+ };
+ 
+
+#endif // _MESHINFO_H
